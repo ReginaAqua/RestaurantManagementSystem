08ce40602793ba1fb92ce875f873bfd96d3b3d93
PhosphorosEosphoros <anastasiosdrog@hotmail.com>
2025-03-26 16:58:12 +0200

    Update Re-enablePassword.php

--- phpFiles/Re-enablePassword.php
+++ phpFiles/Re-enablePassword.php
@@ -1,24 +1,41 @@
 <?php
-
-//include('ForgotPassword.php');
-
-// Initializing a message variable
-$message = "";
-//connecting to MySQL database
-$con = mysqli_connect("localhost","root","","user");
+session_start(); //for cookies
+//Load users json Data
+$json_file = "../Data/users.json";
+$json_data = file_get_contents($json_file);
+$json_dec = json_decode($json_data, true);
 
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieving the password from the user's input
     $newPassword = $_POST['new-password'];
     $confirmPassword = $_POST['confirm-password'];
-
-    // Check if the passwords match
+    $pass=false;
+    // Check if the passwords match and then changing the user's password in the json file. 
     if ($newPassword === $confirmPassword) {
         $message = "<span style='color: green;'> Passwords match!";
-    } else {        
+        foreach($json_dec as $index => $user)
+        {
+            if($_SESSION['email'] == $user['email'])
+            {
+                $hashed_password = password_hash($newPassword, PASSWORD_DEFAULT);
+                $json_dec[$index]['password'] = $hashed_password;
+                $pass=true;
+                break;
+            }
+        }   
+    } 
+    else {        
         $message = "<span style='color: red;'>Passwords do not match. Please try again.</span>";
     }
+    if($pass)
+    {
+        $pass=false;
+        $json_en = json_encode($json_dec, JSON_PRETTY_PRINT);
+        file_put_contents($json_file, $json_en);
+        header("Location: ../htmlfiles/login.html");
+    }
+
 }
 
 
@@ -30,12 +47,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Change Password</title>
-    <link rel="stylesheet" href="../htmlFiles/Re-enablePassword.css">
+    <link rel="stylesheet" href="../cssFiles/Re-enablePassword.css">
 </head>
 <body>
 
     <div class="container">
-        <img src="foto/eyespy.png" alt="Image description" class="image">
+        <img src="../foto/eyespy.png" alt="Image description" class="image">
         <p class="description">Don't mind me, just investigating your password huehuehue.</p>
     </div>
     
