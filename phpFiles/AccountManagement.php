1fcbca39625ceffc1e95ee40d4762c1e87526cf4
PhosphorosEosphoros <anastasiosdrog@hotmail.com>
2025-03-29 13:06:45 +0200

    Update AccountManagement.php

--- phpFiles/AccountManagement.php
+++ phpFiles/AccountManagement.php
@@ -23,34 +23,67 @@ if (file_exists($json)) {
         header("Location: AccountManagement.php");
         exit();
     }
-   //creating  table row with table data to showcase users info: changeable and non chang
-    $userRows = '';
+   //creating  table row with table data to showcase users info: changeable and non changeable if editing or if not
+    $Rows = '';
     foreach ($users as $user) {
         if (isset($_GET['edit']) && isset($user['username']) && $_SESSION['usernm'] == $user['username']) {
-            $userRows .= "<tr>
+            $Rows .= "
                 <form method='POST' action='AccountManagement.php'>
+                    <tr><th>Name</th></tr>
+                    <tr>
                     <td><input type='text' name='name' value='" . htmlspecialchars($user['name']) . "' disabled></td>
+                    </tr>
+                     <tr><th>Surname</th></tr>
+                    <tr> 
                     <td><input type='text' name='surname' value='" . htmlspecialchars($user['surname']) . "' disabled></td>
+                    </tr>
+                     <tr><th>Email</th></tr>
+                    <tr>
                     <td><input type='email' name='email' value='" . htmlspecialchars($user['email']) . "' required></td>
+                    </tr>
+                     <tr><th>Phone Number</th></tr>
+                    <tr>
                     <td><input type='text' name='phone' value='" . htmlspecialchars($user['phone']) . "' required></td>
+                    </tr>
+                     <tr><th>Role</th></tr>
+                    <tr>
+                    <td><input type='text' name='role' value='" . htmlspecialchars($user['role']) . "' disabled></td>
+                    </tr>
+                    <tr>
                     <td>
                         <button type='submit' name='save' class='save-button'>Save</button>
                     </td>
                 </form>
             </tr>";
-        } else {
-            $userRows .= "<tr>
+        } else if ($_SESSION['usernm'] == $user['username']){
+            $Rows .= "<tr>
+                <tr><th>Name</th></tr>
                 <td>" . htmlspecialchars($user['name']) . "</td>
+                </tr>
+                <tr><th>Surname</th></tr>
+                <tr>
                 <td>" . htmlspecialchars($user['surname']) . "</td>
+                </tr>
+                <tr><th>Email</th></tr>
+                <tr>
                 <td>" . htmlspecialchars($user['email']) . "</td>
+                </tr>
+                <tr><th>Phone Number</th></tr>
+                <tr>
                 <td>" . htmlspecialchars($user['phone']) . "</td>
+                </tr>
+                <tr><th>Role</th></tr>
+                <tr>
+                <td>".htmlspecialchars($user['role'])."</td>
+                </tr>
+                <tr>
                 <td><a href='AccountManagement.php?edit=1' class='edit-button'>Edit</a>
-                <a href='Re-enablePassword.php' class='change-password'>Change Password</a></td>
+                <a href='../htmlfiles/OldPassword.html' class='change-password'>Change Password</a></td>
             </tr>";
         }
     }
 } else {
-    $userRows = "<tr><td colspan='5'>Error: User data file not found.</td></tr>";
+    $Rows = "<tr><td colspan='5'>Error: User data file not found.</td></tr>";
 }
 ?>
 
@@ -60,7 +93,7 @@ if (file_exists($json)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Account Management</title>
-    <link rel="stylesheet" href="../cssFiles/AccountManagement.css">
+    <link rel="stylesheet" href="../cssFiles/Management.css">
 </head>
 <body>
     <div class="container">
@@ -70,16 +103,9 @@ if (file_exists($json)) {
         <div class="user-table">
             <table>
                 <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Surname</th>
-                        <th>Email</th>
-                        <th>Phone Number</th>
-                        <th>Actions</th>
-                    </tr>
                 </thead>
                 <tbody>
-                    <?php echo $userRows; ?>
+                    <?php echo $Rows; ?>
                 </tbody>
             </table>
         </div>
